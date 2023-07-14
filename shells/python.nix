5a5129a378754cfedaf38af1dfb17264bc3d69a2
Michael Strobel <mstrobel97@gmail.com>
2023-07-15 00:09:36 +0200

    🐚 add python nix-shell

---
+++ shells/python.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    (pkgs.python311.withPackages (ps: [
+      ps.pip
+      ps.pygame
+    ]))
+  ];
+  shellHook = ''
+    export PIP_PREFIX=$(pwd)/_build/pip_packages
+    export PYHTONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYHTONPATH"
+    export PATH="$PIP_PREFIX/bin:$PATH"
+    unset SOURCE_DATE_EPOCH
+  '';
+}
