f51d0b539b840a54628470d75ab17d037f8e4201
Tobias Betz <tobiasbetz@hotmail.de>
2023-07-18 16:00:02 +0200

    Update hardware-configuration.nix

--- hosts/titania/hardware-configuration.nix
+++ hosts/titania/hardware-configuration.nix
@@ -11,19 +11,13 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
     {
-      device = "/dev/disk/by-label/nixos";
+      device = "/dev/disk/by-uuid/d14eead3-aafa-41a0-a588-aa6281e4ee42";
       fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-label/boot";
-      fsType = "vfat";
     };
 
   swapDevices = [ ];
@@ -34,7 +28,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   # networking.useDHCP = lib.mkDefault true;
   networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
-  networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
+  networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
