292916a6d8ea6726707b437c50b5e6e20614aba4
Michael Strobel <mstrobel97@gmail.com>
2023-07-19 08:55:00 +0200

    remove persistent workspaces 6-9

--- modules/hyprland/home.nix
+++ modules/hyprland/home.nix
@@ -69,22 +69,12 @@ let
     bind = SUPER, 3, workspace, 3
     bind = SUPER, 4, workspace, 4
     bind = SUPER, 5, workspace, 5
-    bind = SUPER, 6, workspace, 6
-    bind = SUPER, 7, workspace, 7
-    bind = SUPER, 8, workspace, 8
-    bind = SUPER, 9, workspace, 9
-    bind = SUPER, 0, workspace, 0
 
     bind = SUPERSHIFT, 1, movetoworkspace, 1
     bind = SUPERSHIFT, 2, movetoworkspace, 2
     bind = SUPERSHIFT, 3, movetoworkspace, 3
     bind = SUPERSHIFT, 4, movetoworkspace, 4
     bind = SUPERSHIFT, 5, movetoworkspace, 5
-    bind = SUPERSHIFT, 6, movetoworkspace, 6
-    bind = SUPERSHIFT, 7, movetoworkspace, 7
-    bind = SUPERSHIFT, 8, movetoworkspace, 8
-    bind = SUPERSHIFT, 9, movetoworkspace, 9
-    bind = SUPERSHIFT, 0, movetoworkspace, 0
 
     bind=,XF86AudioLowerVolume,exec,${pkgs.pamixer}/bin/pamixer -d 10
     bind=,XF86AudioRaiseVolume,exec,${pkgs.pamixer}/bin/pamixer -i 10
